ce0d4dc22f3ec116ca9cffc9388644ea3a5691e9
Chris Ninman <ctsnackpack@gmail.com>
2021-09-29 09:54:36 -0400

    Heading to Chat

--- index.js
+++ index.js
@@ -11,6 +11,41 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+
+const titleCased = (tutorials) => {
+  tutorials.map((arrayStrings) => {
+     const words = arrayStrings.split(' ');
+     debugger
+     console.log(arraystrings)
+      words.map((element) => {
+        return element.charAt(0).toUpperCase() + element.slice(1)});
+  }) .join(' ')
 }
+
+
+//  const tutorial = 'you can do it'
+//  const separate = tutorial.split(' '); 
+//  const capitalize = separate.map((element) => {return element.charAt(0).toUpperCase() + element.slice(1)});
+//  const youCanDoIt = capitalize.join(' ')
+
+
+// const titleCased = (str) =>{
+//   return tutorials.toLowerCase().split(' ').map(function(word) {
+//     return (word.charAt(0).toUpperCase() + word.slice(1));
+//   }).join(' ');
+// }
+// titleCased(tutorials);
+
+//tutorials.map
+  //make a new array after iterating through tutorials array and perfoming function on it
+    //const titleCased = map(tutorials.split(' '), function (...)) {
+      //
+    //}
+
+//     titleCased.
+
+
+
+
+
+
